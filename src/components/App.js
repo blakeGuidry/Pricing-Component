ef38b2606d6ae38b20ffbca600765aed125df569
CoryLHarris <sirrahyroc@gmail.com>
2020-05-14 12:11:04 -0500

    help me

--- src/components/App.js
+++ src/components/App.js
@@ -12,7 +12,7 @@ class App extends Component {
     super(props);
     this.state = {
 
-      sku: '510121', //---> wilson
+      sku: '510122', //---> wilson
       product_info: '',
       isRendered: false,
       price: [],
